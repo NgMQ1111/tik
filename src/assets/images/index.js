673fc6ed7f3bda03710bd35960f69527e8df1d64
ManhQuan <manhquan1096@gmail.com>
2022-08-08 22:41:53 +0700

    new commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,7 @@
+const images = {
+    logo: require('~/assets/images/logo.svg').default,
+    searchBtn: require('~/assets/images/searchBtn.svg').default
+
+}
+
+export default images
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,18 +1,35 @@
-import styles from "./Header.module.scss"
 import classNames from "classnames/bind";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark, faSpinner } from "@fortawesome/free-solid-svg-icons";
+
+import styles from "./Header.module.scss";
+import images from "~/assets/images";
 
 //! thư viện classnames giải quyết vấn đề đặt tên của class trong Component
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
 
 function Header() {
-    return ( 
-        <header className={cx('wrapper')}>
-            <div className={cx('inner')}>
-                {/* Logo */}
-                {/* Sreach */}
-            </div>
-        </header>
-     );
+  return (
+    <header className={cx("wrapper")}>
+      <div className={cx("inner")}>
+        <div className={cx("logo")}>
+          <img src={images.logo} alt="Tiktok" />
+        </div>
+        <div className={cx("sreach")}>
+          <input placeholder="Sreach accounts and videos" spellCheck={false} />
+          <button className={cx("clear")}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
+
+          <button className={cx("sreach-btn")}>
+            <img className="searchBtn" src={images.searchBtn} alt="SearchBtn" />
+          </button>
+        </div>
+        <div className={cx("action")}></div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;

65db974886856f242ec126c72e974a37a92505eb
ManhQuan <manhquan1096@gmail.com>
2022-08-09 10:50:47 +0700

    new commit

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classNames from "classnames/bind";
+import styles from "./AccountItem.module.scss";
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+  return (
+    <div className={cx("wrapper")}>
+      <img
+        className={cx("avatar")}
+        src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/55e797a3e19fe7ce41a4469366acf20d~c5_300x300.webp?x-expires=1660183200&x-signature=Hf7jLEs25j7%2BnvLV5IzweYv8mHE%3D"
+        alt=""
+      />
+      <div className={cx("info")}>
+        <h4 className={cx("name")}>
+          <span>Nguyen Van A</span>
+          <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+        </h4>
+        <span className={cx('username')}>nguyenvana</span>
+      </div>
+    </div>
+  );
+}
+
+export default AccountItem;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,31 +1,67 @@
+import { useEffect, useState } from "react";
+
+import Tippy from "@tippyjs/react/headless";
 import classNames from "classnames/bind";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleXmark, faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 import styles from "./Header.module.scss";
 import images from "~/assets/images";
+import { Wrapper as PopperWrapper } from "~/components/Popper";
+import AccountItem from "~/components/AccountItem";
 
 //! thư viện classnames giải quyết vấn đề đặt tên của class trong Component
 const cx = classNames.bind(styles);
 
 function Header() {
+  const [searchResult, setSearchResult] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSearchResult([]);
+    }, 0);
+  }, []);
+
   return (
     <header className={cx("wrapper")}>
       <div className={cx("inner")}>
         <div className={cx("logo")}>
           <img src={images.logo} alt="Tiktok" />
         </div>
-        <div className={cx("sreach")}>
-          <input placeholder="Sreach accounts and videos" spellCheck={false} />
-          <button className={cx("clear")}>
-            <FontAwesomeIcon icon={faCircleXmark} />
-          </button>
-          <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
 
-          <button className={cx("sreach-btn")}>
-            <img className="searchBtn" src={images.searchBtn} alt="SearchBtn" />
-          </button>
-        </div>
+        <Tippy
+          interactive
+          visible={searchResult.length > 0}
+          render={(attrs) => (
+            <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+              <PopperWrapper>
+                <h4 className={cx('search-title')}>
+                  Accounts
+                </h4>
+                <AccountItem />
+                <AccountItem />
+                <AccountItem />
+                <AccountItem />
+              </PopperWrapper>
+            </div>
+          )}
+        >
+          <div className={cx("sreach")}>
+            <input
+              placeholder="Sreach accounts and videos"
+              spellCheck={false}
+            />
+            <button className={cx("clear")}>
+              <FontAwesomeIcon icon={faCircleXmark} />
+            </button>
+            <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
+
+            <button className={cx("sreach-btn")}>
+              <img src={images.searchBtn} alt="SearchBtn" />
+            </button>
+          </div>
+        </Tippy>
+
         <div className={cx("action")}></div>
       </div>
     </header>

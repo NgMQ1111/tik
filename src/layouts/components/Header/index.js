58ea43f13570ca3416bf678c589ba0b47d4e43b8
ManhQuan <manhquan1096@gmail.com>
2022-08-17 23:24:29 +0700

    new commit

--- src/layouts/components/Header/index.js
+++ src/layouts/components/Header/index.js
@@ -23,7 +23,7 @@ import Menu from "~/components/Popper/Menu";
 import { InboxIcon, MessagesIcon } from "~/components/Icons";
 import Image from "~/components/Images";
 import Search from "../Search";
-import routesConfig from "~/config/routes";
+import config from "~/config";
 
 //! thư viện classnames giải quyết vấn đề đặt tên của class trong Component
 const cx = classNames.bind(styles);
@@ -210,7 +210,7 @@ function Header() {
   return (
     <header className={cx("wrapper")}>
       <div className={cx("inner")}>
-        <Link to={routesConfig.home} className={cx("logo")}>
+        <Link to={config.routes.home} className={cx("logo")}>
           <img src={images.logo} alt="Tiktok" />
         </Link>
 
--- src/layouts/components/Search/index.js
+++ src/layouts/components/Search/index.js
@@ -6,17 +6,15 @@ import styles from "./Search.module.scss";
 import HeadlessTippy from "@tippyjs/react/headless";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faCircleNodes,
   faCircleNotch,
   faCircleXmark,
-  faSpinner,
 } from "@fortawesome/free-solid-svg-icons";
 
 import { Wrapper as PopperWrapper } from "~/components/Popper";
 import AccountItem from "~/components/AccountItem";
 import { SearchIcon } from "~/components/Icons";
 import { useDebounce } from "~/hooks";
-import * as searchServices from "~/apiServices/searchServices";
+import * as searchServices from "~/services/searchServices";
 
 const cx = classNames.bind(styles);
 
